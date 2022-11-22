d3caa74075b95e9543641ba5943fac955e41617f
NgoNhatWin <117475856+ngonhatwin@users.noreply.github.com>
2022-11-22 09:49:19 +0700

    Add files via upload

---
+++ baitaptienbac.cpp
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include "math.h"
+ 
+int main () {
+	
+	#define tien 500
+	
+   int  tien1, tongtien ;
+
+   
+   //giatientrongngay20/11
+   
+   tien1 = tien * 20 / 100  ;
+   
+   
+   //tongsotienkhimua10cuon
+   
+   tongtien= 10 * tien1;
+   
+   printf ("\n tong gia tien 10 cuon la = %d", tongtien);
+   
+}
