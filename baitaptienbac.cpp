4e6c81bf226c1258d2f9053979b440400812c37a
NgoNhatWin <117475856+ngonhatwin@users.noreply.github.com>
2022-11-29 08:54:56 +0700

    Update baitaptienbac.cpp

--- baitaptienbac.cpp
+++ baitaptienbac.cpp
@@ -5,17 +5,19 @@ int main () {
 	
 	#define tien 500
 	
-   int  tien1, tongtien ;
+   int  tien1, tongtien, tien2 ;
 
    
    //giatientrongngay20/11
    
    tien1 = tien * 20 / 100  ;
+	
+	tien2= tien - tien1;
    
    
    //tongsotienkhimua10cuon
    
-   tongtien= 10 * tien1;
+   tongtien= 10 * tien2;
    
    printf ("\n tong gia tien 10 cuon la = %d", tongtien);
    
